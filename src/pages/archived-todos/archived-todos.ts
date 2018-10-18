0426b492bf23c51be0441024686a93f8120c4b04
KGoel <kgoel@nextgen.com>
2018-10-18 14:25:38 +0530

    Added edit feature

---
+++ src/pages/archived-todos/archived-todos.ts
@@ -0,0 +1,32 @@
+import { Component } from '@angular/core';
+import { IonicPage, NavController, NavParams } from 'ionic-angular';
+
+import {TodoProvider} from '../../providers/todo/todo'
+
+/**
+ * Generated class for the ArchivedTodosPage page.
+ *
+ * See https://ionicframework.com/docs/components/#navigation for more info on
+ * Ionic pages and navigation.
+ */
+
+@IonicPage()
+@Component({
+  selector: 'page-archived-todos',
+  templateUrl: 'archived-todos.html',
+})
+export class ArchivedTodosPage {
+
+  public archivedTodos = [];
+  constructor(private todoService: TodoProvider, public navCtrl: NavController, public navParams: NavParams) {
+  }
+
+  ionViewDidLoad() {
+    this.archivedTodos = this.todoService.getArchivedTodos();
+  }
+
+  getArchivedTodos(){
+
+  }
+
+}
--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,5 +1,9 @@
 import { Component } from '@angular/core';
-import { NavController } from 'ionic-angular';
+import { NavController, AlertController, reorderArray, ToastController } from 'ionic-angular';
+
+import {TodoProvider} from '../../providers/todo/todo';
+
+import { ArchivedTodosPage } from '../archived-todos/archived-todos';
 
 @Component({
   selector: 'page-home',
@@ -7,8 +11,89 @@ import { NavController } from 'ionic-angular';
 })
 export class HomePage {
 
-  constructor(public navCtrl: NavController) {
+  public todos = [];
+  public reorderIsEnabled = false;
+  public archivedTodosPage = ArchivedTodosPage;
 
+  constructor(private toastController: ToastController, private todoService: TodoProvider, public navCtrl: NavController, private alertController: AlertController) {
+    this.todos = this.todoService.getTodos();
   }
 
+  toggleReorder(){
+    this.reorderIsEnabled = !this.reorderIsEnabled;
+  }
+
+  itemReorderd($event){
+    reorderArray(this.todos, $event);
+  }
+
+  goToArchivePage(){
+    this.navCtrl.push(ArchivedTodosPage);
+  }
+
+  archiveTodo1(todoIndex){
+    // this.todoService.archiveTodo(todoIndex);
+    this.todoService.archiveTodo(todoIndex);
+  }
+
+ openTodoAlert(){
+    let addTodoAlert = this.alertController.create({
+      title: "Add a Todo",
+      message: "Enter your Todo",
+      inputs:[{
+        type: "text",
+        name: "addTodoInput"
+      }],
+      buttons: [{
+        text: "Cancel"
+      },{
+        text: "Add a Todo",
+        handler: (inputdata) => {
+          let todoText;
+          todoText = inputdata.addTodoInput;
+          // this.todos.push(todoText);
+          this.todoService.addTodo(todoText);
+
+          addTodoAlert.onDidDismiss(() => {
+            let addTodoToast = this.toastController.create({
+              message: "Todo Added",
+              duration: 2000
+            });
+            addTodoToast.present();
+          });
+        }
+      }]
+    });
+    addTodoAlert.present();
+  }
+
+  openEditTodoAlert(todoIndex){
+    let editTodoAlert = this.alertController.create({
+      title: "Edit Todo",
+      message: "Enter you Todo",
+      inputs:[{
+        type: "text",
+        name: "editTodoInput"
+      }],
+      buttons:[{
+        text: "Cancel",
+      }, {
+        text: "Edit Todo",
+        handler: (inputdata) => {
+          let todoText;
+          todoText = inputdata.editTodoInput;
+          this.todoService.editTodo(todoIndex, todoText)
+
+        editTodoAlert.onDidDismiss(() => {
+            let addTodoToast = this.toastController.create({
+              message: "Todo updated",
+              duration: 2000
+            });
+            addTodoToast.present();
+          });
+        }
+      }]
+    });
+    editTodoAlert.present();
+  }
 }
